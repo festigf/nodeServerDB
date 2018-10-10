c3af1c65e664788fa9a0471b0f57ca37796d38b0
festigf <gianfranco.festi@marconirovereto.it>
2018-10-10 16:17:23 +0200

    esempio routing con modulo api

--- routes/api_router.js
+++ routes/api_router.js
@@ -1,18 +1,23 @@
 var express = require("express");
 var ALLOWED_IPS = [
 "127.0.0.1",
-"123.456.7.89"
+"192.168.56.1"
 ];
 var api = express.Router();
 api.use(function(req, res, next) {
+    console.log(req.ip);
     var userIsAllowed = ALLOWED_IPS.indexOf(req.ip) !== -1;
-    if (!userIsAllowed) {
+    /*if (!userIsAllowed) {
         res.status(401).send("Not authorized!");
-    } else {
+    } else
+    */ {
         next();
     }
 });
-api.get("/users", function(req, res) { /* ... */ });
+api.get("/users", function(req, res) { 
+  console.log(req.url);  
+  res.end(req.url);
+});
 api.post("/user", function(req, res) { /* ... */ });
 api.get("/messages", function(req, res) { /* ... */ });
 api.post("/message", function(req, res) { /* ... */ });
